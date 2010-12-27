62121843edbef0f1341ea2b3e1d1188112d2dfa0
rsepulveda <rsepulveda2@gmail.com>
2010-12-27 13:13:24 -0600

    cleanup source code

--- app/controllers/rsakey_controller.rb
+++ app/controllers/rsakey_controller.rb
@@ -1,13 +1,12 @@
 class RsakeyController < ApplicationController
-	RSALength = 128
-	
-	def index
-		key_pair = RSA::KeyPair.generate(RSALength)
-		session[:key_pair] = key_pair
-		session[:e] = [ key_pair.public_key.exponent, "%x" % [key_pair.public_key.exponent] ]
-		session[:n] = [ key_pair.public_key.modulus, "%x" % [key_pair.public_key.modulus] ]
-		session[:d] = [ RSALength, "%x" % [RSALength] ]
-	end
-	
-	private
+  RSALength = 128
+
+  def index
+    key_pair = RSA::KeyPair.generate(RSALength)
+    
+    session[:key_pair] = key_pair
+    session[:e] = [ key_pair.public_key.exponent, "%x" % [key_pair.public_key.exponent] ]
+    session[:n] = [ key_pair.public_key.modulus, "%x" % [key_pair.public_key.modulus] ]
+    session[:d] = [ RSALength, "%x" % [RSALength] ]
+  end
 end
